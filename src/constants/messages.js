6512e089f9531e1cbf4cf00cbc9e9dcb54a6a141
Akshay <akshay.singh@bee-logical.com>
2025-02-13 12:28:13 +0530

    Added constants

---
+++ src/constants/messages.js
@@ -0,0 +1,13 @@
+exports.messages = {
+    INTERNAL_SERVER_ERROR: 'Internal server error',
+    USER_NOT_FOUND: 'User not found',
+    INVALID_CREDENTIALS: 'Invalid email or password',
+    INVALID_TOKEN: 'Invalid token',
+    BAD_REQUEST: 'Bad request',
+    SUCCESS_MESSAGE: 'Success',
+    UPDATE_MESSAGE: 'UPDATED',
+    BAD_REQUEST: 'Bad request',
+    EMAIL_ALREADY_EXISTS: 'Email already exists',
+    NOT_FOUND: 'Not found',
+    // Add more messages as needed
+  };
