c41b02ed283d90a7e9c9324d53756a8063e1ad16
Sushil <sushilshinde9158@gmail.com>
2025-02-13 14:22:31 +0530

    Added OTP sending and verification API

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const authController = require("./controllers/authController");
 const app = express();
 
 const PORT = 3000;
@@ -10,6 +11,33 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.send("Hello, Express!");
 });
+
+app.post('/send-otp', (req, res) => {
+
+    const {email} = req.body;
+
+    authController.sendEmail(email).then((result) => {
+        console.log('email: ', email);
+        res.status(200).send({token: result});
+    })
+
+})
+
+app.post("/verify-otp", (req, res) => {
+
+    const {otp, encryptedData} = req.body;
+
+    authController.verifyOTP(otp, encryptedData).then((verified) => {
+        if(verified) {
+            res.status(200).send({message: 'Otp verified!'});
+        }else{
+            res.status(401).send({message: 'Otp verification failed!'});
+        }
+    })
+
+})
+
+
 
 // Start server
 app.listen(PORT, () => {
